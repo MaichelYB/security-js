9249d7cb3ff54fd5bc52811254d49abb9525d648
Maichel Yunarto <maichely@yahoo.com>
2024-05-11 15:56:02 +0700

    remove index trial error

--- src/decrypt.js
+++ src/decrypt.js
@@ -2,7 +2,7 @@ import { privateDecrypt, constants } from 'crypto';
 
 export function Decrypt(privateKey, dataToDecrypt) {
     if (privateKey == "" || dataToDecrypt == "") {
-        console.log("empty public key or data to encrypt")
+        console.log("empty private key or data to encrypt")
         return
     }
 
@@ -12,6 +12,6 @@ export function Decrypt(privateKey, dataToDecrypt) {
         padding: constants.RSA_PKCS1_PADDING
     }, Buffer.from(dataToDecrypt, 'base64'));
 
-    console.log('Encrypted data in Node.js:', dataToDecrypt.toString('utf8'));
-    return dataToDecrypt.toString('utf8')
+    console.log('Decrypted data in Node.js:', decryptedData.toString('utf8'));
+    return decryptedData.toString('utf8')
 }
